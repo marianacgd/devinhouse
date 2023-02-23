0c3c883bc95323c1e5503953e2d55ce0e396f35c
Mariana <mariana.daruix@gmail.com>
2023-02-23 10:40:17 -0300

    atualiza prj star-wars

--- M01/m01s08/star-wars/src/app/app.component.ts
+++ M01/m01s08/star-wars/src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   img = 'assets/leia.jpg';
   title = 'Star Wars - Leia';
   text = '"Ela tinha uma personalidade forte e grande intelecto, tendo realizado muito em sua juventude: ela foi um proponente importante para a Aliança Rebelde, a mais jovem Senadora do Império Galáctico com dezoito anos de idade, além de ser membra da Família Real de Alderaan. Anos depois, ela foi instrumental na criação da Nova República, sendo eleita para a posição de Chefe de Estado, servindo duas vezes nesse posto."';
+  info = 'Humana, Feminino, 1.5'
 }
--- M01/m01s08/star-wars/src/components/filho/filho.component.ts
+++ M01/m01s08/star-wars/src/components/filho/filho.component.ts
@@ -9,9 +9,10 @@ export class FilhoComponent {
   @Input() public foto = '';
   @Input() public titulo = '';
   @Input() public texto = '';
+  @Input() public infos = '';
 
   public fotoLuke = 'https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/12/luke-star-wars.jpg'
   public tituloLuke = 'Luke Skywalker'
-  public textoLuke = 'texto luke'
-
+  public textoLuke = '"Eu lutei as piores das guerras, e testemunhei a redenção do mal. Eu vi o equilíbrio ser restaurado à Força. Mas a ordem pode se tornar o caos… como aconteceu quando eu nasci. Agora, com aqueles que amo e meus leais aliados, enfrento um novo desafio como nenhum outro antes. E não tenho certeza se desta vez podemos vencer."'
+  public infosLuke = 'Humano, Masculino, 1.72'
 }
